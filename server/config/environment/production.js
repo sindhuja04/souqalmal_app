e1f8f406ecb5dd79947f7afa5a62886f9243f2db
Sindhuja Ravi <sindhuja.ravicafe@gmail.com>
2016-04-20 01:40:16 +0530

    Added Mongo Lab Sandbox url.

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -19,6 +19,6 @@ module.exports = {
           process.env.MONGOHQ_URL ||
           process.env.OPENSHIFT_MONGODB_DB_URL +
           process.env.OPENSHIFT_APP_NAME ||
-          'mongodb://localhost/desktop'
+          'mongodb://sindhuja:sindhuja@ds013891.mlab.com:13891/souqalmal'
   }
 };
