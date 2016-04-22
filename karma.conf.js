a803e727e814875da95c564c2bcda93ef199eec5
Sindhuja Ravi <sindhuja.ravicafe@gmail.com>
2016-04-22 22:10:27 +0530

    Deploy to heroku

--- karma.conf.js
+++ karma.conf.js
@@ -22,6 +22,7 @@ module.exports = function(config) {
       'client/bower_components/angular-ui-router/release/angular-ui-router.js',
       'client/bower_components/angular-validation-match/dist/angular-validation-match.min.js',
       'client/bower_components/angularUtils-pagination/dirPagination.js',
+      'client/bower_components/angular-filter/dist/angular-filter.min.js',
       'client/bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'client/app/app.js',
